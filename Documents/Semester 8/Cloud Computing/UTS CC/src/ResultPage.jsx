47ede7cd63a3e8a8cf4175a60321bb1ce327e4a2
Amadeus <amadeus.gavriel.tataming-2020@ftmm.unair.ac.id>
2024-03-24 23:20:31 +0700

    v3 dev

--- Documents/Semester 8/Cloud Computing/UTS CC/src/ResultPage.jsx
+++ Documents/Semester 8/Cloud Computing/UTS CC/src/ResultPage.jsx
@@ -3,30 +3,36 @@ import { Box, Grid, GridItem, Heading, Image, VStack, Center } from '@chakra-ui/
 
 function ResultPage() {
   // Placeholder image URLs
-  const cheek1Url = "https://via.placeholder.com/150";
-  const cheek2Url = "https://via.placeholder.com/150";
-  const chinUrl = "https://via.placeholder.com/150";
-  const foreheadUrl = "https://via.placeholder.com/150";
-  const fullFaceUrl = "https://via.placeholder.com/628";
+  const cheek1Url = "https://via.placeholder.com/200";
+  const cheek2Url = "https://via.placeholder.com/200";
+  const chinUrl = "https://via.placeholder.com/200";
+  const foreheadUrl = "https://via.placeholder.com/200";
+  const fullFaceUrl = "https://via.placeholder.com/412";
 
   return (
     <Box minHeight='100vh' bg='black' p='5'>
-      <Center mb='4'>
+      <Center mb='8'>
         <Heading color='white' size='3xl'>
           Your Personalized Result
         </Heading>
       </Center>
-      <Grid templateColumns="1fr 2fr" gap={6} alignItems="center">
-        {/* Grid for smaller images */}
-        <VStack spacing={2} py='5'>
-          <Image src={cheek1Url} alt="Cheek 1" boxSize="150px" objectFit="cover" />
-          <Image src={cheek2Url} alt="Cheek 2" boxSize="150px" objectFit="cover" />
-          <Image src={chinUrl} alt="Chin" boxSize="150px" objectFit="cover" />
-          <Image src={foreheadUrl} alt="Forehead" boxSize="150px" objectFit="cover" />
+      <Grid templateColumns="1fr 2fr 1fr" gap={6} alignItems="center" mx='180px'>
+        {/* Grid for smaller images on the left */}
+        <VStack spacing={3}>
+          <Image src={cheek1Url} alt="Cheek 1" boxSize="200px" objectFit="cover" />
+          <Image src={cheek2Url} alt="Cheek 2" boxSize="200px" objectFit="cover" />
         </VStack>
 
         {/* Main picture */}
-        <Image src={fullFaceUrl} alt="Full Face" boxSize="628px" objectFit="cover" />
+        <Center>
+          <Image src={fullFaceUrl} alt="Full Face" boxSize="412px" objectFit="cover" />
+        </Center>
+
+        {/* Grid for smaller images on the right */}
+        <VStack spacing={2}>
+          <Image src={chinUrl} alt="Chin" boxSize="200px" objectFit="cover" />
+          <Image src={foreheadUrl} alt="Forehead" boxSize="200px" objectFit="cover" />
+        </VStack>
       </Grid>
     </Box>
   );
